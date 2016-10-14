5d0add7c71515f9ab046d53d07a59a72240eb6c2
Benjamin Smith <benjamin-smith@hotmail.com>
2016-10-14 17:43:10 -0400

    Login now inserts user into database. Needs to check for existing user before creating a new one.

--- controllers/main_controller.js
+++ controllers/main_controller.js
@@ -8,4 +8,15 @@ router.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname+'/../views/main/index.html'));
 });
 
+router.post('/login', function (req, res) {
+	console.log(JSON.stringify(req.body));
+	models.User.create({
+		username: req.body.uname,
+		password: req.body.psw
+	})
+	.then(function () {
+		res.redirect('/donation');
+	});
+});
+
 module.exports = router;
