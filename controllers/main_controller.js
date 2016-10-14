565fe61c98fe1c6d6fd0b60a1049ec6387216a4f
Benjamin Smith <benjamin-smith@hotmail.com>
2016-10-14 18:05:22 -0400

    Checks for user before creating a new one. Redirect to donations now includes user ID.

--- controllers/main_controller.js
+++ controllers/main_controller.js
@@ -10,12 +10,29 @@ router.get('/', function(req, res) {
 
 router.post('/login', function (req, res) {
 	console.log(JSON.stringify(req.body));
-	models.User.create({
-		username: req.body.uname,
-		password: req.body.psw
+	models.User.findOne({
+		where: {
+			username: {
+				$eq: req.body.uname
+			}
+		}
 	})
-	.then(function () {
-		res.redirect('/donation');
+	.then (function (user) {
+		if (!user) {
+			models.User.create({
+				username: req.body.uname,
+				password: req.body.psw
+			})
+			.then(function (newUser) {
+				console.log(JSON.stringify(newUser));
+				res.redirect('/donation?user=' + newUser.id);
+			});
+		}
+		else {
+			// We should probably check passwords, but it's for charity :).
+			console.log(JSON.stringify(user));
+			res.redirect('/donation?user=' + user.id);
+		}
 	});
 });
 
