b557fe437ef485555febd1b2b137ee868c2727d3
bootcamp <bootcamp@Jordans-MacBook-Pro-FLHI.local>
2016-10-14 21:53:04 -0400

    added handlebars

--- controllers/report_controller.js
+++ controllers/report_controller.js
@@ -7,5 +7,21 @@ var path = require('path');
 router.get('/', function(req, res) {
   res.sendFile(path.join(__dirname+'/../views/report/index.html'));
 });
+router.get('/:userId', function(req, res) {
+		models.Donation.findAll({
+		where: {
+			UserId: {
+				$eq: req.params.userId
+			}
+		}
+	}).then(function(Donation) {
+    // grab the user info from our req.
+    // How is it in our req?
+    // This info gets saved to req via the users_controller.js file.
+    res.render('report/index', {
+    	Donation : Donation
+    });
+  });
+});
 
 module.exports = router;
